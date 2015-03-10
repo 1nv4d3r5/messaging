ac61b69870881231163e9b95f669889d96c877ca
DrRoach <'ryandeas1@gmail.com'>
2015-03-10 00:21:44 +0000

    Use POST and generate hash data

--- user/register.php
+++ user/register.php
@@ -5,47 +5,46 @@ class register
     public static function call($con)
     {
         //Avoiding creating unnecessary notices
-        $username = (isset($_GET['username']) ? $_GET['username'] : null);
-        $email = (isset($_GET['email']) ? $_GET['email'] : null);
-        $password = (isset($_GET['password']) ? $_GET['password'] : null);
-        $confPassword = (isset($_GET['confPassword']) ? $_GET['confPassword'] : null);
+        $username = (isset($_POST['username']) ? $_POST['username'] : null);
+        $email = (isset($_POST['email']) ? $_POST['email'] : null);
+        $password = (isset($_POST['password']) ? $_POST['password'] : null);
+        $confPassword = (isset($_POST['confPassword']) ? $_POST['confPassword'] : null);
 
         //Username MUST be at least 4 characters long
         if (strlen($username) < 4) {
-            echo json_encode(array(
-                'status' => 'ERROR',
-                'message' => 'Sorry, your username must be at least 4 characters long'
-            ));
-            exit;
+			throw new Exception('Sorry, your username must be at least 4 characters long', 400);
         }
 
         //Check to make sure that both passwords match
         if($password != $confPassword) {
-            echo json_encode(array(
-                'status' => 'ERROR',
-                'message' => 'Please make sure that your password and confirmation passwords match'
-            ));
-            exit;
+			throw new Exception('Please make sure that your password and confirmation passwords match', 400);
         }
 
         //Check to make sure that the username is not already in use
         $usernameCheck = mysqli_query($con, 'SELECT id FROM users WHERE username="'.$username.'"');
         while($row = mysqli_fetch_assoc($usernameCheck)) {
-            echo json_encode(array(
-                'status' => 'ERROR',
-                'message' => 'Sorry, that username has already been taken'
-            ));
-            exit;
+			throw new Exception('Sorry, that username has already been taken', 400);
         }
 
         //Check to make sure that the email address isn't already in use
         $emailCheck = mysqli_query($con, 'SELECT id FROM users WHERE email="'.$email.'"');
         while($row = mysqli_fetch_assoc($emailCheck)) {
-            echo json_encode(array(
-                'status' => 'ERROR',
-                'message' => 'Sorry, that email is already in use'
-            ));
-            exit;
+			throw new Exception('Sorry, that email is already in use', 400);
         }
+		
+		//Register user in backend API
+		$salt = self::generateSalt();
+		$pepper = functions::getPepper();
+		$hash = functions::generateHash([
+			'password' => $password,
+			'salt' => $salt,
+			'pepper' => $pepper
+		]);
+		
+		return ['Salt = ' . $salt . ' Pepper = ' . $pepper . ' Hash = ' .$hash];
     }
+	
+	public static function generateSalt() {
+		return md5(microtime());
+	}
 }
